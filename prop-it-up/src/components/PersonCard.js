0db3cd2ed095cbaeeab8218d958af4d9421a8806
debughunt <huntercollinray@gmail.com>
2022-05-06 11:59:04 -0400

    changed to functional comps

--- prop-it-up/src/components/PersonCard.js
+++ prop-it-up/src/components/PersonCard.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class PersonCard extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            age : this.props.age
-        }
-    }
-    render(){
-        let { firstName, lastName, age, hairColor } = this.props
-
-        const increaseAge = () => {
-            this.setState({age: this.state.age + 1})
-        }
+const PersonCard = props => {
         return (
             <div>
-                <h1>{ lastName }, { firstName }</h1>
-                <p>Age: { this.state.age}</p>
-                <p>Hair Color: { hairColor }</p>
-                <button onClick={increaseAge}>Birthday button for {firstName} {lastName}</button>
+                <h1>{ props.lastName }, { props.firstName }</h1>
+                <p>Age: { props.age}</p>
+                <p>Hair Color: { props.hairColor }</p>
             </div>
         );
-    }
 }
+
 
 export default PersonCard;
